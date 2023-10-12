2e1400c452e0a4e09719d1537fd3885f89d7d68f
Mehul Makwana <mehul.makwana@quantifeed.com>
2023-10-13 09:00:37 +1100

    create model for storing custom spelling collection

---
+++ SpellingMAUI/Spelling.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace SpellingMAUI;
+
+public class Spelling
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Words { get; set; }
+}
--- SpellingMAUI/UserScores.cs
+++ SpellingMAUI/UserScores.cs
@@ -1,19 +1,13 @@
 ﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SpellingMAUI
+namespace SpellingMAUI;
+
+public class UserScores
 {
-    public class UserScores
-    {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-        public int Correct { get; set; }
-        public int InCorrect { get; set; }
-        public string Time { get; set; }
-        public bool Done { get; set; }
-    }
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public int Correct { get; set; }
+    public int InCorrect { get; set; }
+    public string Time { get; set; }
+    public bool Done { get; set; }
 }
