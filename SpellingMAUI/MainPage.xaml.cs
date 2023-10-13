38362b37e23dd2acc21f7235ae66964c3800b515
Mehul Makwana <mehul.makwana@quantifeed.com>
2023-10-13 14:35:17 +1100

    - avoid repeating words - at the end of list, save the progress

--- SpellingMAUI/MainPage.xaml.cs
+++ SpellingMAUI/MainPage.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Timers;
+﻿using System.Collections.Generic;
+using System.Timers;
 using Timer = System.Threading.Timer;
 
 namespace SpellingMAUI;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
     int correct = 0;
     int incorrect = 0;
     string currentWord = null;
@@ -17,13 +17,18 @@ public partial class MainPage : ContentPage
     CancellationTokenSource cts;
     string wordsStr = "intense, determine, deny, presentation, nucleus, neutral, intellectual, approach, circular, observe, literature, rotten, exposure, gesture, abstract, substantial, conservation, scent, Budget, alternative, competent, output, damp, potential, combine, oral, agricultural, association, critical, division, distribution, strategy, capacity, management, contest, preparation, prosperity, beneath, opponent, breed, launch, slave, electric, measure, manufacturer, equivalent, sensible, puzzle, virtually, widespread, bend, acceptance, signal, regulation, survive, prohibit, grand, definition, attain, define, evidence, substance, establish, welfare, propose, unite, publication, comparison, external, universal, requirement, characteristic, code, pulse, resource, statistics, operate, variable, faculty, statistical, efficiency, executive, operation, motivate, represent, diverse, criticism, concentration, enthusiasm, hence, participate, philosopher, suitable, rational, questionnaire, emotional, administration, precise, temperature, logical, tendency, accord, crowd, conference, atmosphere, genuine, concern, Republic, personnel, claim, Republican, structure, pack, complex, candidate, impressive, politics, filter, response, retain, reduction, excess, electrical, maintain, efficient, digital, span, application, interfere, commercial, possession, zone, evolve, flash, interpretation, convey, revolution, phrase, engage, peasant, racial, entertainment, magnificent, benefit, security, successfully, locate, concept, reveal, emerge, reform, essential, track, bang, outcome, threaten, exploit, impose, researcher, bind, molecule, magnetic, photograph, oxygen, trail, schedule, reproduce, sponsor, threat, implication, legend, democracy, marine, appliance, perfume, explosive, identify, unconscious, resemble, lean, estimate, recognise, photographic, stroke, density, demonstrate, agriculture, unique, publish, throughout, initial, trace, vibration, scale, sticky, refine, hatch, incident, flexible, criteria";
 
-    public List<string> Words { get; set; }
+    private List<string> Words { get; set; }
 
     public MainPage(TodoItemDatabase todoItemDatabase)
     {
         InitializeComponent();
-        Words = wordsStr.Split(',').Select(x => x.Trim()).ToList();
+
         database = todoItemDatabase;
+    }
+
+    private void LoadSpellings()
+    {
+        Words = wordsStr.Split(',').Select(x => x.Trim()).Take(1).ToList();
     }
 
     private async void TxtSpell_Completed(object sender, EventArgs e)
@@ -49,17 +54,22 @@ public partial class MainPage : ContentPage
                 incorrectWords.Add(currentWord);
             }
         }
+
         AnswerLbl.Text = currentWord;
-        ResultLbl.Text = $"Correct: {correct} | Incorrect: {incorrect} | Current Position: {count}";
+        ResultLbl.Text = $"Correct: {correct} | Incorrect: {incorrect}";
 
         TxtSpell.Text = "";
 
         currentWord = GetRandomWord();
-        await SpeakNowDefaultSettingsAsync(currentWord);
+        if (!string.IsNullOrEmpty(currentWord))
+        {
+            await SpeakNowDefaultSettingsAsync(currentWord);
+        }
     }
 
     private async void SpeakBtn_Clicked(object sender, EventArgs e)
     {
+        LoadSpellings();
         currentWord = GetRandomWord();
         SpeakBtn.IsEnabled = false;
         StopBtn.IsEnabled = true;
@@ -71,8 +81,16 @@ public partial class MainPage : ContentPage
 
     private string GetRandomWord()
     {
-        count = random.Next(Words.Count);
-        return Words[count];
+        if (Words.Count == 0)
+        {
+            StopBtn_Clicked(null, null);
+            return null;
+        }
+        int index = random.Next(0, Words.Count);
+        var word = Words[index];
+        Words.RemoveAt(index);
+
+        return word;
     }
 
     private void StartTimer()
@@ -142,7 +160,7 @@ public partial class MainPage : ContentPage
         currentWord = null;
 
         // reset stats
-        correct = incorrect = count = 0;
+        correct = incorrect = 0;
         ResultLbl.Text = "";
         ResultLbl.IsVisible = AnswerLbl.IsVisible = false;
         incorrectWords.Clear();
